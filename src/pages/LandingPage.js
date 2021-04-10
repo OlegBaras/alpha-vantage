082dac6b3430b766d0a62a0a9f802d933c45e61f
Oleg Baras <tbaras@gmail.com>
2021-04-10 12:45:53 +0100

    styled landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import history from "../components/History";
-import "../css/LandingPage.css";
 import { randomStringGenerator } from "../components/randomStringGenerator";
+import "../css/LandingPage.css";
 
 function LandingPage() {
   const [apiKey, setApiKey] = useState("");
@@ -9,34 +9,51 @@ function LandingPage() {
   function handleInput(e) {
     setApiKey(e.target.value);
   }
+  function handleKeyDown(e) {
+    if (e.key === "Enter") {
+      if (apiKey.length > 4) {
+        e.preventDefault();
+        localStorage.setItem("apiKey", apiKey);
+        localStorage.setItem("SearchKeyWord", "");
+        history.push("search");
+      } else {
+        return;
+      }
+    }
+  }
 
-  // Recording apiKey value to localStorage for future use
   function handleSubmit(e) {
     e.preventDefault();
     localStorage.setItem("apiKey", apiKey);
     localStorage.setItem("SearchKeyWord", "");
     history.push("search");
   }
+
   const generateRandom = () => {
-    console.log("random");
     let randomSymbol = randomStringGenerator(10);
     localStorage.setItem("apiKey", randomSymbol);
     localStorage.setItem("SearchKeyWord", "");
     history.push("search");
-    console.log(randomSymbol);
   };
 
   return (
     <div className="landing-page">
-      {/* <form> */}
-      <div className="landing-content">
-        <h2>Input API Key:</h2>
+      <h2 className="title">Welcome to Alpha Visible Project</h2>
+      <div className="form">
         <input
           type="text"
           name="API_KEY"
           value={apiKey}
           onChange={handleInput}
+          onKeyDown={handleKeyDown}
+          autoComplete="off"
+          required
         />
+        <label for="API_KEY" className="label-name">
+          <span className="content-name">
+            INPUT API KEY (minimum 5 characters)
+          </span>
+        </label>
         <button
           disabled={apiKey.length < 5}
           type="submit"
@@ -44,10 +61,18 @@ function LandingPage() {
         >
           Submit
         </button>
-        {/* </form> */}
       </div>
-      <div className="example-key">
-        <button onClick={() => generateRandom()}>or use example key</button>
+      <div className="separator">
+        <div className="or-spacer">
+          <span>
+            <i>or</i>
+          </span>
+        </div>
+      </div>
+      <div className="example">
+        <button className="example-button" onClick={() => generateRandom()}>
+          Click here to use example key
+        </button>
       </div>
     </div>
   );
