76a2f801566dd5507d6b11eed4a82ea3f39580f1
Oleg Baras <tbaras@gmail.com>
2021-04-26 22:37:27 +0100

    WIP: Daily, Weekly, Monthly views done

--- src/components/ItemDetails/Daily.js
+++ src/components/ItemDetails/Daily.js
@@ -1,82 +1,137 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-balham.css";
+import "../../css/Intraday.css";
 
-function Daily({ company, value }) {
+export default function Daily({ company, value }) {
   const API_KEY = localStorage.getItem("apiKey");
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
-  const [metaData, setMetaData] = useState({});
-  const [timeSeries, setTimeSeries] = useState({});
   const [note, setNote] = useState();
+  const [rowData, setRowData] = useState(null);
+  const [gridApi, setGridApi] = useState();
+  const [gridColumnApi, setGridColumnApi] = useState();
+
+  const defaultColdDef = {
+    flex: 1,
+    sortable: true,
+    filter: true,
+    resizable: true,
+  };
+
+  const columnDefs = [
+    {
+      headerName: "Open",
+      field: "open",
+    },
+    {
+      headerName: "High",
+      field: "high",
+    },
+    {
+      headerName: "Low",
+      field: "low",
+    },
+    {
+      headerName: "Close",
+      field: "close",
+    },
+    {
+      headerName: "Volume",
+      field: "volume",
+    },
+  ];
+
+  const renameKey = (obj, oldKey, newKey) => {
+    obj[newKey] = obj[oldKey];
+    delete obj[oldKey];
+  };
+
   useEffect(() => {
     const fetchItem = async () => {
       setIsError(false);
       setIsLoading(true);
-      let unmounted = false;
-
       try {
-        if (!unmounted) {
-          const result = await axios(
-            `https://www.alphavantage.co/query?function=${value}&symbol=${company}&apikey=${API_KEY}`
-            // { timeout: 100 }
-          );
+        const result = await axios(
+          `https://www.alphavantage.co/query?function=${value}&symbol=${company}&apikey=${API_KEY}`
+        );
+        if (result.data["Note"]) {
+          setNote(result.data["Note"]);
+          console.log("noteeeeee");
+          setRowData([]);
+        } else {
+          const obj = result.data["Time Series (Daily)"];
+          const myData = Object.keys(obj).map((key) => {
+            return obj[key];
+          });
 
-          setMetaData(result.data["Meta Data"]);
-          setTimeSeries(result.data["Time Series (Daily)"]);
-          setNote(result.data.Note);
+          myData.forEach((obj) => {
+            renameKey(obj, "1. open", "open");
+            renameKey(obj, "2. high", "high");
+            renameKey(obj, "3. low", "low");
+            renameKey(obj, "4. close", "close");
+            renameKey(obj, "5. volume", "volume");
+          });
+          console.log("myData", myData);
+
+          const updatedJson = myData;
+
+          setRowData(updatedJson);
         }
       } catch (error) {
-        if (!unmounted) {
-          setIsError(true);
-        }
+        setIsError(true);
       }
       setIsLoading(false);
-      return () => {
-        unmounted = true;
-      };
     };
     fetchItem();
   }, []);
 
+  function gridReadyHandler(params) {
+    setGridApi(params.api);
+    setGridColumnApi(params.columnApi);
+  }
+
+  if (note) {
+    return (
+      <div>
+        You have used API calls limit, please wait a minute and refresh to
+        continue.
+      </div>
+    );
+  }
+
+  if (isError) {
+    return <div> Something went wrong</div>;
+  }
+
+  if (isLoading) {
+    return <div>Loading....</div>;
+  }
+
   return (
     <div>
-      {note && <div>{note}</div>}
-      <p>{company}</p> <p>{value}</p>
-      {isError && <div>Something went wrong </div>}
-      {isLoading ? (
-        <div> Loading...</div>
-      ) : (
-        <div>
-          <div>
-            {metaData ? (
-              Object.keys(metaData).map(function (key, index) {
-                return (
-                  <div key={index}>
-                    {key} : {metaData[key]}
-                  </div>
-                );
-              })
-            ) : (
-              <>no meta data</>
-            )}
+      <div className="table">
+        {/* {isError && <div>Something went wrong...</div>} */}
+        {/* {isLoading && <div className="loading">Loading...</div>} */}
+        {rowData && (
+          <div className="result-list">
+            <div
+              className="ag-theme-balham"
+              style={{ width: "80%", height: "65vh" }}
+            >
+              <AgGridReact
+                // rowStyle={rowStyle}
+                onGridReady={gridReadyHandler}
+                columnDefs={columnDefs}
+                rowData={rowData}
+                defaultColDef={defaultColdDef}
+              />
+            </div>
           </div>
-          <div className="time-series">
-            {timeSeries ? (
-              Object.keys(timeSeries).map(function (key, index) {
-                return (
-                  <div key={index}>
-                    {key} : open : {timeSeries[key]["1. open"]}
-                    {key} : close : {timeSeries[key]["4. close"]}
-                  </div>
-                );
-              })
-            ) : (
-              <>no time series data</>
-            )}
-          </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 }
-export default Daily;
