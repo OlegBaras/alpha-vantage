f9bf9e82f7ec366e10d3c3e69ab164243ca769a6
Oleg Baras <tbaras@gmail.com>
2021-04-28 23:24:15 +0100

    search component loading conditionals

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "../App.css";
 import "../css/Search.css";
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/dist/styles/ag-grid.css";
@@ -9,18 +8,19 @@ import history from "../components/History";
 import Nav from "../components/Nav";
 import Loading from "../components/Loading";
 import Note from "../components/Note";
+import SymbolNotFoundNote from "../components/SymbolNotFoundNote";
 
 function Search() {
   const API_KEY = localStorage.getItem("apiKey");
   const [isError, setIsError] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-  const [searchKeyWord, setSearchKeyWord] = useState("");
+  const [searchKeyWord, setSearchKeyWord] = useState("ibm");
   const [url, setUrl] = useState(
     `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${searchKeyWord}&apikey=${API_KEY}`
   );
   const [gridApi, setGridApi] = useState();
   const [gridColumnApi, setGridColumnApi] = useState();
-  const [rowData, setRowData] = useState(null);
+  const [rowData, setRowData] = useState([]);
   const [note, setNote] = useState();
 
   const defaultColdDef = {
@@ -74,22 +74,16 @@ function Search() {
     delete obj[oldKey];
   };
   useEffect(() => {
-    //check if there is search key word at all
+    //first thing to check if there is search keyword
     if (searchKeyWord) {
-      console.log("render times");
       const fetchItems = async () => {
         setIsError(false);
         setIsLoading(true);
-        console.log("isLoading", isLoading);
-
         try {
           const result = await axios(url);
-
           if (result.data["Note"]) {
             setNote(result.data["Note"]);
             setRowData([]);
-            console.log("rowData", rowData);
-            console.log("note", note);
           } else {
             const arr = result.data.bestMatches;
             arr.forEach((obj) => {
@@ -107,7 +101,6 @@ function Search() {
             setRowData(updatedJson);
           }
         } catch (error) {
-          console.log("error", error);
           setIsError(true);
         }
         setIsLoading(false);
@@ -150,7 +143,7 @@ function Search() {
   }
 
   return (
-    <div className="search-window">
+    <div className="search-page">
       <Nav />
       <div className="root">
         <div className="form">
@@ -177,22 +170,25 @@ function Search() {
         <div className="table">
           {isError ? <div>Something went wrong...</div> : null}
           {isLoading ? <Loading /> : null}
-          {note ? <Note /> : null}
-          {rowData ? (
+          {note && !isLoading ? <Note /> : null}
+          {rowData.length && !isError && !isLoading && !note ? (
             <div className="result-list">
               <div
                 className="ag-theme-balham"
                 style={{ width: "80%", height: 315 }}
               >
                 <AgGridReact
-                  onGridReady={gridReadyHandler}
                   columnDefs={columnDefs}
                   rowData={rowData}
                   onCellClicked={() => history.push(`/search/${searchKeyWord}`)}
                   defaultColDef={defaultColdDef}
+                  onGridReady={gridReadyHandler}
                 />
               </div>
             </div>
+          ) : null}
+          {!rowData.length && !isLoading && !note ? (
+            <SymbolNotFoundNote />
           ) : null}
         </div>
       </div>
